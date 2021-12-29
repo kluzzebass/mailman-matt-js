c34f1dceef27b776d4c2712a4804bd44b6d3c11a
Jan Fredrik Leversund <kluzz@radical.org>
2021-12-29 14:51:26 +0100

    removed unused code

--- main.js
+++ main.js
@@ -96,18 +96,6 @@ const monthLookup = {
   oktober: 9,
   november: 10,
   desember: 11
-  // januar: 1,
-  // februar: 2,
-  // mars: 3,
-  // april: 4,
-  // mai: 5,
-  // juni: 6,
-  // juli: 7,
-  // august: 8,
-  // september: 9,
-  // oktober: 10,
-  // november: 11,
-  // desember: 12
 }
 
 const parseDateStr = (dateStr) => {
